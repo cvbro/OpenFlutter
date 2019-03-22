49a322244702129a8904ffd9dfd3f54a3dfc4766
ztluo <ztluo.job@gmail.com>
2019-03-22 16:32:19 +0800

    :heavy_plus_sign: react-router

--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
 
 class App extends Component {
 
   render() {
-    return <div>Hello react</div>;
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about/">About</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+        </div>
+      </Router>
+    )
   }
 }
 
