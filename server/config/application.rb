0820e49c7a845b9a2a14beb126158d872f3fa633
ztluo <ztluo.job@gmail.com>
2019-03-22 20:33:37 +0800

    :heavy_plus_sign: add kimurai as web crawler

--- server/config/application.rb
+++ server/config/application.rb
@@ -31,5 +31,7 @@ module OpenFlutter
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
