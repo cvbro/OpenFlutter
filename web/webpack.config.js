1569bd794b83961a0facfb131627d0f7c53124d8
ztluo <ztluo.job@gmail.com>
2019-03-27 12:13:01 +0800

    :heavy_plus_sign: sass-loader css-loader style-loader

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -6,7 +6,9 @@ module.exports = {
   plugins: [new HtmlWebpackPlugin()],
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.scss$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] },
+      { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader'] }
     ],
   },
 }
