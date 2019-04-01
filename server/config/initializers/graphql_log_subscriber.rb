b1ec5ca4f456f6de0c022a2c9945b82ff15b3aff
ztluo <ztluo.job@gmail.com>
2019-04-01 17:50:01 +0800

    :sparkles: server: better logger format for graphql query

--- server/config/initializers/graphql_log_subscriber.rb
+++ server/config/initializers/graphql_log_subscriber.rb
@@ -1,27 +1,23 @@
 require "action_controller/log_subscriber"
 
-class GraphqlLogSubscriber < ActionController::LogSubscriber
-  INTERNAL_PARAMS = %w(controller action format _method only_path)
-  def start_processing(event)
+module GraphqlLogSubscriber
+  def start_processing(event=nil)
     return unless logger.info?
 
     payload = event.payload
-    params  = payload[:params].except(*INTERNAL_PARAMS)
+    params  = payload[:params].except(*ActionController::LogSubscriber::INTERNAL_PARAMS)
     format  = payload[:format]
     format  = format.to_s.upcase if format.is_a?(Symbol)
 
     info "Processing by #{payload[:controller]}##{payload[:action]} as #{format}"
     return if params.empty?
     if payload[:controller] == 'GraphqlController' and payload[:action] == 'execute'
-      info color("Graphql: --->", MAGENTA, false)
-      info color("#{params[:query]}#{params[:variables]}", MAGENTA, true)
-      info color("<---", MAGENTA, false)
+      info color("Graphql: ", ActiveSupport::LogSubscriber::MAGENTA, false)
+      info color("#{params[:query]}\n#{params[:variables]}", ActiveSupport::LogSubscriber::MAGENTA, true)
     else
       info "  Parameters: #{params.inspect}"
     end
   end
 end
 
-
-ActiveSupport::Notifications.notifier.unsubscribe "start_processing.action_controller"
-GraphqlLogSubscriber.attach_to :action_controller
+ActionController::LogSubscriber.prepend GraphqlLogSubscriber
