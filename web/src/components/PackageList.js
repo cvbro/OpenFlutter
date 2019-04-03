37455be220f191f2f82507d6bf3a4a7d38f28476
ztluo <ztluo.job@gmail.com>
2019-04-03 18:06:47 +0800

    :sparkles: web: play video with progressbar

--- web/src/components/PackageList.js
+++ web/src/components/PackageList.js
@@ -1,20 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
+import Player from 'react-player'
+import ProgressBar from '../components/ProgressBar'
+const Line = ProgressBar.Line
 
 import Loading from './Loading'
 import Error from './Error'
 
-const PackageListItem = ({item}) => (
-  <li className="main-item fl">
-    <div className="main-img"></div>
-    <div className="main-title">{item.name}</div>
-    <div className="limit clear">
-      <span className="fl"></span>
-      <span className="fl"><i class="fab fa-github"></i></span>
-    </div>
-  </li>
-)
+const PackageListItem = ({item}) => {
+
+  const [playing, setPlaying] = useState(false)
+  const [progress, setProgress] = useState(0)
+
+
+  return (
+    <li className="main-item fl">
+      <Player
+        className="main-img"
+        height="475px"
+        width="264px"
+        url={item.video}
+        playing={playing}
+        poster={item.image}
+        onMouseOver={()=>setPlaying(true)}
+        onMouseOut={()=>setPlaying(false)}
+        progressInterval={500}
+        onProgress={({played})=> setProgress(played)}
+        />
+        <Line
+          progress={progress}
+          options={{strokeWidth: 1}}
+          />
+      <div className="main-title">{item.name}</div>
+      <div className="limit clear">
+        <a target='_blank' href={item.pubUrl} className="fl"><i className="fab fa-github" /></a>
+        <a target='_blank' href={item.repositoryUrl} className="fl"><i className="fab fa-github" /></a>
+      </div>
+    </li>
+  )
+}
 
 const PackageList = ({list}) => (
   <ul className="main-list clear">
@@ -27,6 +52,8 @@ const query = gql`
   packages {
     id
     name
+    image
+    video
     pubUrl
     repositoryUrl
   }
